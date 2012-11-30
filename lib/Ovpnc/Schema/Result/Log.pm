67a31d802a00266bf7533562e776e452380acd27
Nuriel Shem-Tov <nuri@de-bar.com>
2012-11-30 19:27:39 +0100

    Start working on logging into DB

---
+++ lib/Ovpnc/Schema/Result/Log.pm
@@ -0,0 +1,124 @@
+use utf8;
+package Ovpnc::Schema::Result::Log;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Ovpnc::Schema::Result::Log
+
+=cut
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components("InflateColumn::DateTime");
+
+=head1 TABLE: C<log>
+
+=cut
+
+__PACKAGE__->table("log");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 message
+
+  data_type: 'integer'
+  is_nullable: 0
+
+=head2 user_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 timestamp
+
+  data_type: 'timestamp'
+  datetime_undef_if_invalid: 1
+  default_value: current_timestamp
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "message",
+  { data_type => "integer", is_nullable => 0 },
+  "user_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "timestamp",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    default_value => \"current_timestamp",
+    is_nullable => 0,
+  },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 user
+
+Type: belongs_to
+
+Related object: L<Ovpnc::Schema::Result::User>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "user",
+  "Ovpnc::Schema::Result::User",
+  { id => "user_id" },
+  { is_deferrable => 1, on_delete => "NO ACTION", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07033 @ 2012-11-30 16:51:01
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:+4Ru1zcFrb0inmdsVGC4kg
+
+__PACKAGE__->load_components("InflateColumn::DateTime","TimeStamp");
+
+__PACKAGE__->add_columns(
+   timestamp => { data_type => 'datetime', set_on_create => 1 }
+);
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->meta->make_immutable;
+1;
