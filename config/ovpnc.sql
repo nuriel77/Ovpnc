716c79b38d164a8ab64ab08c9eacd5b61b972c08
Nuriel Shem-Tov <nuri@de-bar.com>
2013-01-02 12:36:05 +0100

    Updated the sql template

--- config/ovpnc.sql
+++ config/ovpnc.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 06, 2012 at 12:14 AM
+-- Generation Time: Jan 02, 2013 at 12:32 PM
 -- Server version: 5.1.63-0+squeeze1-log
--- PHP Version: 5.3.18-1~dotdeb.0
+-- PHP Version: 5.3.19-1~dotdeb.0
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,17 +23,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `certificates`
+--
+
+CREATE TABLE IF NOT EXISTS `certificates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `user` varchar(72) NOT NULL,
+  `name` varchar(72) NOT NULL,
+  `created_by` varchar(72) NOT NULL,
+  `key_cn` varchar(42) NOT NULL,
+  `key_org` text NOT NULL,
+  `key_ou` text NOT NULL,
+  `key_country` varchar(2) NOT NULL,
+  `key_province` varchar(128) NOT NULL,
+  `key_city` varchar(128) NOT NULL,
+  `key_size` smallint(8) NOT NULL,
+  `key_expire` int(4) NOT NULL,
+  `key_email` varchar(40) NOT NULL,
+  `key_serial` char(2) NOT NULL,
+  `revoked` datetime NOT NULL,
+  `cert_type` varchar(12) NOT NULL,
+  `cert_file` text NOT NULL,
+  `key_file` text NOT NULL,
+  `cert_digest` varchar(40) NOT NULL,
+  `key_digest` varchar(40) NOT NULL,
+  `created` date NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=224 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `log`
 --
 
 CREATE TABLE IF NOT EXISTS `log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `message` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `username` varchar(72) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  KEY `user_id` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=88 ;
 
 -- --------------------------------------------------------
 
@@ -59,6 +93,19 @@ INSERT INTO `roles` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` char(72) NOT NULL,
+  `session_data` text,
+  `expires` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -79,14 +126,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Ovpnc Users for OpenVPN' AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Ovpnc Users for OpenVPN' AUTO_INCREMENT=134 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `enabled`, `username`, `password`, `password_expires`, `fullname`, `email`, `phone`, `address`, `revoked`, `created`, `modified`) VALUES
-(1, 1, 'ovpncadmin', '$2$14$XBz98gwkk1vI/YeUyQmma.6nZJZdu0Y94/joNXWxdi.74s/KKrpXK', '2019-01-01 00:00:00', 'Ovpnc Administrator', 'ovpnc@localhost', '0000', '0000', 0, '2012-11-28 05:20:00', '2012-12-05 23:13:30');
+INSERT INTO `users` (`id`, `enabled`, `username`, `password`, `salt`, `password_expires`, `fullname`, `email`, `phone`, `address`, `revoked`, `created`, `modified`) VALUES
+(1, 1, 'ovpncadmin', '$2$14$XBz98gwkk1vI/YeUyQmma.6nZJZdu0Y94/joNXWxdi.74s/KKrpXK', '', '2019-01-01 00:00:00', 'Ovpnc Administrator', 'ovpnc@localhost', '0000', '0000', 0, '2012-11-28 05:20:00', '2012-12-31 16:45:08');
 
 -- --------------------------------------------------------
 
@@ -113,10 +160,10 @@ INSERT INTO `user_roles` (`user_id`, `role_id`) VALUES
 --
 
 --
--- Constraints for table `log`
+-- Constraints for table `certificates`
 --
-ALTER TABLE `log`
-  ADD CONSTRAINT `log_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `certificates`
+  ADD CONSTRAINT `certificates_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `user_roles`
