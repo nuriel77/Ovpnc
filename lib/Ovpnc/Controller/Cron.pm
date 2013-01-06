668b76c608e30cd9a29750e67efc1d96238e4e6f
Nuriel Shem-Tov <nuri@de-bar.com>
2013-01-06 05:38:49 +0100

    Added comments to Cron controller

--- lib/Ovpnc/Controller/Cron.pm
+++ lib/Ovpnc/Controller/Cron.pm
@@ -6,10 +6,39 @@ use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
+=head1 NAME
+
+Ovpnc::Controller::Cron - Ovpnc Cron jobs
+
+=head1 DESCRIPTION
+
+Here functions will be placed
+to be called from Ovpnc.pm
+to run at different times
+or process via manual triggers
+
+=head1 METHODS
+
+=cut
+
+
+=head2 remove_sessions
+
+Remove expired sessions
+
+=cut
+
 	sub remove_sessions : Private {
         my ( $self, $c ) = @_;      
 		$c->delete_expired_sessions;
     }
+
+
+=head1 AUTHOR
+
+Nuriel Shem-Tov
+
+=cut
 
 __PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 
