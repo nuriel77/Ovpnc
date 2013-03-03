b5518067c08d23a1929301d059c87c3bfa3d53be
Nuriel Shem-Tov <nuri@de-bar.com>
2013-03-03 21:50:55 +0100

    Mod to log pm and deps of makefile

--- lib/Ovpnc/Schema/Result/Log.pm
+++ lib/Ovpnc/Schema/Result/Log.pm
@@ -1,36 +1,18 @@
-use utf8;
 package Ovpnc::Schema::Result::Log;
 
 # Created by DBIx::Class::Schema::Loader
 # DO NOT MODIFY THE FIRST PART OF THIS FILE
 
-=head1 NAME
-
-Ovpnc::Schema::Result::Log
-
-=cut
-
 use strict;
 use warnings;
 
-use Moose;
-use MooseX::NonMoose;
-use MooseX::MarkAsMethods autoclean => 1;
-extends 'DBIx::Class::Core';
-
-=head1 COMPONENTS LOADED
-
-=over 4
-
-=item * L<DBIx::Class::InflateColumn::DateTime>
-
-=back
-
-=cut
+use base 'DBIx::Class::Core';
 
 __PACKAGE__->load_components("InflateColumn::DateTime");
 
-=head1 TABLE: C<log>
+=head1 NAME
+
+Ovpnc::Schema::Result::Log
 
 =cut
 
@@ -58,7 +40,6 @@ __PACKAGE__->table("log");
 =head2 timestamp
 
   data_type: 'timestamp'
-  datetime_undef_if_invalid: 1
   default_value: current_timestamp
   is_nullable: 0
 
@@ -73,35 +54,23 @@ __PACKAGE__->add_columns(
   { data_type => "varchar", is_nullable => 0, size => 72 },
   "timestamp",
   {
-    data_type => "timestamp",
-    datetime_undef_if_invalid => 1,
+    data_type     => "timestamp",
     default_value => \"current_timestamp",
-    is_nullable => 0,
+    is_nullable   => 0,
   },
 );
-
-=head1 PRIMARY KEY
-
-=over 4
-
-=item * L</id>
-
-=back
-
-=cut
-
 __PACKAGE__->set_primary_key("id");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07025 @ 2013-01-02 12:37:18
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:MvIDz9324xrprAFEvQSxBA
+# Created by DBIx::Class::Schema::Loader v0.07000 @ 2013-03-03 21:43:06
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:jhkSrXmRcu4+woH9TQeIHg
 
 __PACKAGE__->load_components("InflateColumn::DateTime","TimeStamp");
 
 __PACKAGE__->add_columns(
-   timestamp => { data_type => 'datetime', set_on_create => 1 }
+   modified => { data_type => 'datetime',   set_on_create => 1 },
+   created  => { data_type => 'date',       set_on_create => 1 },
 );
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
-__PACKAGE__->meta->make_immutable;
+# You can replace this text with custom content, and it will be preserved on regeneration
 1;
